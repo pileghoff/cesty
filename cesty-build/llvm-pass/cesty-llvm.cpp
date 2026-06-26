b6e7c0c440f13ed4c375c34bde05ad92ca89f394
Pil Eghoff <pil.eghoff@gmail.com>
2026-06-26 13:55:32 +0200

    Cleanup llvm pass and reduce overhead

--- cesty-build/llvm-pass/cesty-llvm.cpp
+++ cesty-build/llvm-pass/cesty-llvm.cpp
@@ -1,123 +1,137 @@
 #include "llvm/IR/DataLayout.h"
+#include "llvm/IR/Function.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/raw_ostream.h"
-
+#include <tuple>
+#include <vector>
 using namespace llvm;
 
 namespace {
 
-class CestyPass : public PassInfoMixin<CestyPass> {
+class CestyFuncHandler {
 public:
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
+  CestyFuncHandler() = delete;
+  IRBuilder<> EntryBuilder;
+  std::vector<std::tuple<Type *, AllocaInst *>> allocs;
+  Function &F;
+  FunctionCallee LoadFunc;
+  FunctionCallee StoreFunc;
+  const DataLayout &DL;
+  CestyFuncHandler(Function &F)
+      : EntryBuilder(&F.getEntryBlock(), F.getEntryBlock().begin()), F(F),
+        DL(F.getParent()->getDataLayout()) {
 
-    Module *M = F.getParent();
-    LLVMContext &Ctx = M->getContext();
+    LoadFunc = F.getParent()->getOrInsertFunction(
+        "cesty_load",
+        FunctionType::get(Type::getVoidTy(F.getParent()->getContext()),
+                          {EntryBuilder.getPtrTy(), EntryBuilder.getPtrTy(),
+                           EntryBuilder.getInt64Ty()},
+                          false));
 
+    StoreFunc = F.getParent()->getOrInsertFunction(
+        "cesty_store",
+        FunctionType::get(Type::getVoidTy(F.getParent()->getContext()),
+                          {EntryBuilder.getPtrTy(), EntryBuilder.getPtrTy(),
+                           EntryBuilder.getInt64Ty()},
+                          false));
     SmallVector<StoreInst *> Stores;
     SmallVector<LoadInst *> Loads;
 
     for (BasicBlock &BB : F) {
       for (Instruction &I : BB) {
-        if (auto *SI = dyn_cast<StoreInst>(&I))
+        if (auto *SI = dyn_cast<StoreInst>(&I)) {
           Stores.push_back(SI);
+        }
 
-        if (auto *LI = dyn_cast<LoadInst>(&I))
+        if (auto *LI = dyn_cast<LoadInst>(&I)) {
           Loads.push_back(LI);
+        }
       }
     }
+
     for (LoadInst *LI : Loads) {
-
-      Module *M = LI->getModule();
-      LLVMContext &Ctx = M->getContext();
-      const DataLayout &DL = M->getDataLayout();
-
-      Value *Src = LI->getPointerOperand();
-      Type *Ty = LI->getType();
-
-      // Allocate temp storage in entry block
-      IRBuilder<> EntryBuilder(&F.getEntryBlock(), F.getEntryBlock().begin());
-
-      AllocaInst *Tmp = EntryBuilder.CreateAlloca(Ty, nullptr, "cesty.tmp");
-
-      // Insert replacement at original load site
-      IRBuilder<> Builder(LI);
-
-      uint64_t Size = DL.getTypeStoreSize(Ty);
-
-      Value *SizeVal = ConstantInt::get(Type::getInt64Ty(Ctx), Size);
-
-      // Declare:
-      // void cesty_load(ptr src,
-      //                 ptr dst,
-      //                 i64 size)
-      FunctionCallee Hook = M->getOrInsertFunction(
-          "cesty_load",
-          FunctionType::get(
-              Type::getVoidTy(Ctx),
-              {Builder.getPtrTy(), Builder.getPtrTy(), Builder.getInt64Ty()},
-              false));
-
-      // Call runtime
-      Builder.CreateCall(Hook, {Src, Tmp, SizeVal});
-
-      // Load from temp
-      LoadInst *Replacement = Builder.CreateLoad(Ty, Tmp, "cesty.loaded");
-
-      // Redirect uses
-      LI->replaceAllUsesWith(Replacement);
-
-      // Remove original load
-      LI->eraseFromParent();
+      handle_load(LI);
     }
 
     for (StoreInst *SI : Stores) {
+      handle_store(SI);
+    }
+  }
 
-      Module *M = SI->getModule();
-      LLVMContext &Ctx = M->getContext();
-      const DataLayout &DL = M->getDataLayout();
-
-      Value *Dst = SI->getPointerOperand();
-      Value *Val = SI->getValueOperand();
-
-      Type *Ty = Val->getType();
-
-      // Create temp storage in function entry block
-      IRBuilder<> EntryBuilder(&F.getEntryBlock(), F.getEntryBlock().begin());
-
-      AllocaInst *Tmp = EntryBuilder.CreateAlloca(Ty, nullptr, "cesty.tmp");
-
-      // Insert replacement at original store location
-      IRBuilder<> Builder(SI);
-
-      // Write value into temp
-      Builder.CreateStore(Val, Tmp);
-
-      // Compute byte size
-      uint64_t Size = DL.getTypeStoreSize(Ty);
-
-      Value *SizeVal = ConstantInt::get(Type::getInt64Ty(Ctx), Size);
-
-      // Declare:
-      // void cesty_store(ptr dst,
-      //                  ptr src,
-      //                  i64 size)
-      FunctionCallee Hook = M->getOrInsertFunction(
-          "cesty_store",
-          FunctionType::get(
-              Type::getVoidTy(Ctx),
-              {Builder.getPtrTy(), Builder.getPtrTy(), Builder.getInt64Ty()},
-              false));
-
-      // Call runtime
-      Builder.CreateCall(Hook, {Dst, Tmp, SizeVal});
-
-      // Remove original store
-      SI->eraseFromParent();
+  AllocaInst *getAlloca(Type *Ty) {
+    for (int i = 0; i < allocs.size(); i++) {
+      auto entry = allocs[i];
+      if (std::get<0>(entry) == Ty) {
+        return std::get<1>(entry);
+      }
     }
 
+    AllocaInst *Tmp = EntryBuilder.CreateAlloca(Ty, nullptr, "cesty.tmp");
+    allocs.push_back({Ty, Tmp});
+    return Tmp;
+  }
+
+  void handle_load(LoadInst *LI) {
+    Value *Src = LI->getPointerOperand();
+    Type *Ty = LI->getType();
+
+    // Insert replacement at original load site
+    IRBuilder<> Builder(LI);
+
+    AllocaInst *Tmp = getAlloca(Ty);
+    uint64_t Size = DL.getTypeStoreSize(Ty);
+
+    Value *SizeVal =
+        ConstantInt::get(Type::getInt64Ty(LI->getModule()->getContext()), Size);
+
+    // Call runtime
+    Builder.CreateCall(LoadFunc, {Src, Tmp, SizeVal});
+
+    // Load from temp
+    LoadInst *Replacement = Builder.CreateLoad(Ty, Tmp, "cesty.loaded");
+
+    // Redirect uses
+    LI->replaceAllUsesWith(Replacement);
+
+    // Remove original load
+    LI->eraseFromParent();
+  }
+
+  void handle_store(StoreInst *SI) {
+
+    Value *Dst = SI->getPointerOperand();
+    Value *Val = SI->getValueOperand();
+
+    Type *Ty = Val->getType();
+
+    // Insert replacement at original store location
+    IRBuilder<> Builder(SI);
+
+    // Write value into temp
+    AllocaInst *Tmp = getAlloca(Ty);
+    Builder.CreateStore(Val, Tmp);
+
+    // Compute byte size
+    uint64_t Size = DL.getTypeStoreSize(Ty);
+    Value *SizeVal =
+        ConstantInt::get(Type::getInt64Ty(SI->getModule()->getContext()), Size);
+
+    // Call StoreFunc
+    Builder.CreateCall(StoreFunc, {Dst, Tmp, SizeVal});
+
+    // Remove original store
+    SI->eraseFromParent();
+  }
+};
+
+class CestyPass : public PassInfoMixin<CestyPass> {
+public:
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &) {
+
+    CestyFuncHandler handler(F);
     return PreservedAnalyses::none();
   }
 
