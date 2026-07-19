395c6d0abfa5a04697c5cfb25945965574999d4a
Pil Eghoff <pil.eghoff@gmail.com>
2026-07-19 14:11:39 +0200

    Use the Default trait for return values, instead of panicking

--- src/lib.rs
+++ src/lib.rs
@@ -4,46 +4,50 @@ pub use cesty_macro::mock;
 pub use lazy_static::lazy_static;
 pub mod mem_mock;
 pub mod shared_state;
-use std::ffi::{CStr, c_char};
-use std::io::{self, Write};
-use std::sync::atomic::{AtomicI8, Ordering};
-use yansi::Paint;
 
-#[unsafe(no_mangle)]
-extern "C" fn cesty_panic(function: *const c_char) {
-    let func = AtomicI8::new(0);
-    std::panic::set_hook(Box::new(move |info| {
-        let info = info.payload_as_str().unwrap_or("missing payload");
-        if func.fetch_add(1, Ordering::Relaxed) == 0 {
-            _ = std::io::stderr().write_fmt(format_args!(
-                "\n\n{}: Called auto-stubbed function {}\n\n",
-                "Panic".bold().red(),
-                info.bold(),
-            ));
-        }
-    }));
+pub mod cesty_panic {
+    use std::ffi::{CStr, c_char};
+    use std::io::Write;
+    use std::sync::atomic::{AtomicI8, Ordering};
+    use yansi::Paint;
 
-    let function = unsafe { CStr::from_ptr(function) };
-    panic!("{:?}", function);
+    #[unsafe(no_mangle)]
+    extern "C" fn cesty_panic(function: *const c_char) {
+        let func = AtomicI8::new(0);
+        std::panic::set_hook(Box::new(move |info| {
+            let info = info.payload_as_str().unwrap_or("missing payload");
+            if func.fetch_add(1, Ordering::Relaxed) == 0 {
+                _ = std::io::stderr().write_fmt(format_args!(
+                    "\n\n{}: Called auto-stubbed function {}\n\n",
+                    "Panic".bold().red(),
+                    info.bold(),
+                ));
+            }
+        }));
+
+        let function = unsafe { CStr::from_ptr(function) };
+        panic!("{:?}", function);
+    }
 }
 
 unsafe impl<Tin, Tout> Send for FunctionMockInner<Tin, Tout>
 where
     Tin: Sized + 'static + Clone,
-    Tout: Sized + 'static + Clone,
+    Tout: Sized + 'static + Clone + Default,
 {
 }
-pub struct FunctionMockInner<Tin: Sized + 'static + Clone, Tout: Sized + 'static + Clone> {
+pub struct FunctionMockInner<Tin: Sized + 'static + Clone, Tout: Sized + 'static + Clone + Default>
+{
     pub call_history: Vec<Tin>,
     pub return_val: std::collections::VecDeque<Tout>,
-    pub handler: Option<Box<dyn std::ops::Fn(Tin) -> Tout>>,
+    pub handler: Option<Box<dyn std::ops::FnMut(Tin) -> Tout>>,
     pub default_ret_val: Option<Tout>,
 }
 
 impl<Tin, Tout> Default for FunctionMockInner<Tin, Tout>
 where
     Tin: Sized + 'static + Clone,
-    Tout: Sized + 'static + Clone,
+    Tout: Sized + 'static + Clone + Default,
 {
     fn default() -> Self {
         Self::new(None)
@@ -53,9 +57,9 @@ where
 impl<Tin, Tout> FunctionMockInner<Tin, Tout>
 where
     Tin: Sized + 'static + Clone,
-    Tout: Sized + 'static + Clone,
+    Tout: Sized + 'static + Clone + Default,
 {
-    pub fn new(handler: Option<Box<dyn std::ops::Fn(Tin) -> Tout>>) -> Self {
+    pub fn new(handler: Option<Box<dyn std::ops::FnMut(Tin) -> Tout>>) -> Self {
         FunctionMockInner {
             call_history: Vec::new(),
             return_val: std::collections::VecDeque::new(),
@@ -64,12 +68,12 @@ where
         }
     }
 
-    pub fn set_handler(&mut self, handler: Option<Box<dyn std::ops::Fn(Tin) -> Tout>>) {
+    pub fn set_handler(&mut self, handler: Option<Box<dyn std::ops::FnMut(Tin) -> Tout>>) {
         self.handler = handler;
     }
 
     pub fn handle(&mut self, input: Tin) -> Tout {
-        if let Some(handler) = &self.handler {
+        if let Some(handler) = &mut self.handler {
             return handler(input);
         }
         self.call_history.push(input);
@@ -81,20 +85,20 @@ where
             Some(v) => v,
             None => match &self.default_ret_val {
                 Some(v) => v.clone(),
-                None => panic!("Unexpected call"),
+                None => Tout::default(),
             },
         }
     }
 }
 
-pub struct FunctionMock<'a, Tin: Sized + 'static + Clone, Tout: Sized + 'static + Clone> {
+pub struct FunctionMock<'a, Tin: Sized + 'static + Clone, Tout: Sized + 'static + Clone + Default> {
     inner: &'a std::sync::Mutex<FunctionMockInner<Tin, Tout>>,
 }
 
 impl<'a, Tin, Tout> FunctionMock<'a, Tin, Tout>
 where
     Tin: Sized + 'static + Clone,
-    Tout: Sized + 'static + Clone,
+    Tout: Sized + 'static + Clone + Default,
 {
     pub fn new(inner: &'a std::sync::Mutex<FunctionMockInner<Tin, Tout>>) -> Self {
         inner.lock().unwrap().call_history.clear();
@@ -104,7 +108,7 @@ where
         FunctionMock { inner }
     }
 
-    pub fn handler(&self, handler: Box<dyn std::ops::Fn(Tin) -> Tout>) {
+    pub fn handler(&self, handler: Box<dyn std::ops::FnMut(Tin) -> Tout>) {
         self.inner.lock().unwrap().set_handler(Some(handler));
     }
 
