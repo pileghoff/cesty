8f8fdcbe320820080f48d9bcf5c5784de3efd70a
Pil Eghoff <pil.eghoff@gmail.com>
2026-08-02 19:49:23 +0200

    Ensure that we handle poisoned mutex in cesty test runner

--- src/test_runner.rs
+++ src/test_runner.rs
@@ -3,9 +3,9 @@ use std::sync::{Mutex, OnceLock};
 static CESTY_TEST_LOCK: OnceLock<Mutex<()>> = OnceLock::new();
 
 pub fn cesty_run_test_internal(test_body: impl FnOnce()) {
-    let _lock = CESTY_TEST_LOCK
-        .get_or_init(|| Mutex::new(()))
-        .lock()
-        .unwrap();
+    let _lock = match CESTY_TEST_LOCK.get_or_init(|| Mutex::new(())).lock() {
+        Ok(m) => m,
+        Err(e) => e.into_inner(),
+    };
     test_body()
 }
